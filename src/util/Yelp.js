af2f2cda25b8b021d6879a3fdf30f0fa38c339fc
Mia <micaelasgilbert@gmail.com>
2021-02-24 01:56:05 -0600

    create Yelp API functionality to replace hard coded business data simulation to complete stage 4

---
+++ src/util/Yelp.js
@@ -0,0 +1,31 @@
+const apiKey = 'aTIErdHUumZn6k59HdQ7CrLZ0W3dStnCaj-XnFcKqjr0TA5oUaVdxGskzd2dnBubfAAnHzKlUSm7KhECEhKDqp6jdIDe_ILQdwyVTCwB2I98WJ7a0lV4f5aBWO01YHYx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+            headers: {Authorization: `Bearer ${apiKey}`}
+        })
+        .then(response => response.json())
+        .then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city, 
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                })
+            }
+        })
+    }
+};
+
+export default Yelp;
